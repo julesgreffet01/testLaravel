f53fab38476e059e3292927daee25b12339b8b47
julesgreffet01 <julesgreffet@gmail.com>
2024-09-12 14:17:30 +0200

    jules

--- resources/views/bonjour.blade.php
+++ resources/views/bonjour.blade.php
@@ -1,1 +1,15 @@
-bonjour
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+<p>username: <strong><?php echo $username;?></strong></p>
+<p>mdp :<strong>{{$mdp}}</strong></p>  <!-- equivalent a au dessus -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,12 @@ Route::get('/', function () {
 
 
 Route::get('/home', function () {
-    return view('bonjour');
+   $username = 'Jules';
+   $mdp = 1234;
+   //envoyer ces donnes a la vue
+    //return view('bonjour',['username' => $username, 'mdp' => $mdp]);
+    //on donne le nom de ce qu on veut puis la valeur et c est le nom qui fait office de nom pour la vue
+    return view ('bonjour', compact('username','mdp'));
 });
+
+
